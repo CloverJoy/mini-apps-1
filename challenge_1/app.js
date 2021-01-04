923e44a3c99cdc3e4cf209df752cd3e839fe1d9f
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-04 14:35:45 -0800

    Bare minimum requirement done.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,8 +3,30 @@ let boardState =
 '', '', '',
 '','',''];
 let message = document.getElementById('message');
+let endgameMessage = document.getElementById('endgame');
 
 let turnCounter = 0;
+let playerOneWin = false;
+let playerTwoWin = false;
+let isDraw = false;
+
+const refreshBoard = () => {
+  boardState =
+  ['', '', '',
+  '', '', '',
+  '','',''];
+  turnCounter = 0;
+  playerOneWin = false;
+  playerTwoWin = false;
+  isDraw = false;
+  for (let i = 0; i <= 8; i++) {
+    let current = document.getElementById(i);
+    current.innerHTML = '';
+  }
+  message.innerHTML = 'Your Turn: Player 1';
+  endgameMessage.innerHTML = '';
+  gameStart();
+}
 
 const playerOneTurn = (i) => {
   message.innerHTML = 'Your Turn: Player 2';
@@ -14,7 +36,10 @@ const playerOneTurn = (i) => {
   console.log(boardState);
   turnCounter ++;
   checkDraw();
-  checkWinCon();
+  checkWinCon()
+  if (isEndGame()) {
+    endGame();
+  };
 }
 
 
@@ -27,6 +52,9 @@ const playerTwoTurn = (i) => {
   turnCounter ++;
   checkDraw();
   checkWinCon();
+  if (isEndGame()) {
+    endGame();
+  }
 }
 
 const checkWinCon = () => {
@@ -39,6 +67,7 @@ const checkWinCon = () => {
       boardState[0] + boardState[4] + boardState[8] === 'XXX' ||
       boardState[2] + boardState[4] + boardState[6] === 'XXX') {
     message.innerHTML = 'PLAYER ONE WIN!'
+    playerOneWin = true;
     return;
   } else if (boardState[0] + boardState[1] + boardState[2] === 'OOO' ||
   boardState[3] + boardState[4] + boardState[5] === 'OOO' ||
@@ -49,6 +78,7 @@ const checkWinCon = () => {
   boardState[0] + boardState[4] + boardState[8] === 'OOO' ||
   boardState[2] + boardState[4] + boardState[6] === 'OOO') {
     message.innerHTML = 'PLAYER TWO WIN!'
+    playerTwoWin = true;
     return;
   }
 };
@@ -56,34 +86,37 @@ const checkWinCon = () => {
 const checkDraw = () => {
   if (turnCounter === 9) {
     message.innerHTML ='DRAW!';
+    isDraw = true;
   }
 };
 
-const playerTurn = () => {
+const isEndGame = () => {
+  if (playerOneWin || playerTwoWin || isDraw) {
+    return true;
+  }
+  return false;
+}
+
+const endGame = () => {
+  endgameMessage.innerHTML = 'Game Over! Click me for new game!'
+  endgameMessage.addEventListener('click', (event) => {
+    refreshBoard();
+  });
+}
+
+const gameStart = () => {
   for (let i = 0; i <= 8; i++) {
     var current = document.getElementById(i);
     current.addEventListener('click', (event) => {
-      if (!event.target.innerText && turnCounter % 2 === 0) {
-        // console.log('Player 1 Fired!' + i);
-        // event.target.innerHTML = 'X';
-        // boardState[i] = 'X';
-        // console.log(boardState);
-        // turnCounter ++;
+      if (isEndGame()) {
+        endGame();
+      } else if (!event.target.innerText && turnCounter % 2 === 0) {
         playerOneTurn(i);
       } else if (!event.target.innerText && turnCounter % 2 === 1) {
-        // console.log('Player 2 fired!' + i);
-        // event.target.innerHTML = 'O';
-        // boardState[i] = 'O';
-        // console.log(boardState);
-        // turnCounter ++;
         playerTwoTurn(i);
       }
     });
   }
-}
-
-const gameStart = () => {
-  playerTurn();
 }
 
 gameStart();
