1fcb179636f048933a8b4fe707c0c0b09da0e4ee
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-04 13:29:16 -0800

    Tic tac toe rule and wincon complete with the helper function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,24 +2,62 @@ let boardState =
 ['', '', '',
 '', '', '',
 '','',''];
+let message = document.getElementById('message');
 
 let turnCounter = 0;
 
 const playerOneTurn = (i) => {
-  console.log('Player 1 Fired! Board: ' + i);
+  message.innerHTML = 'Your Turn: Player 2';
+  console.log('Player 1 Fired! turn: ' + turnCounter);
   event.target.innerHTML = 'X';
   boardState[i] = 'X';
   console.log(boardState);
   turnCounter ++;
+  checkDraw();
+  checkWinCon();
 }
 
+
 const playerTwoTurn = (i) => {
-  console.log('Player 2 Fired! Board: ' + i);
+  message.innerHTML = 'Your Turn: Player 1';
+  console.log('Player 2 Fired! turn: ' + turnCounter);
   event.target.innerHTML = 'O';
   boardState[i] = 'O';
   console.log(boardState);
   turnCounter ++;
+  checkDraw();
+  checkWinCon();
 }
+
+const checkWinCon = () => {
+  if (boardState[0] + boardState[1] + boardState[2] === 'XXX' ||
+      boardState[3] + boardState[4] + boardState[5] === 'XXX' ||
+      boardState[6] + boardState[7] + boardState[8] === 'XXX' ||
+      boardState[0] + boardState[3] + boardState[6] === 'XXX' ||
+      boardState[1] + boardState[4] + boardState[7] === 'XXX' ||
+      boardState[2] + boardState[5] + boardState[8] === 'XXX' ||
+      boardState[0] + boardState[4] + boardState[8] === 'XXX' ||
+      boardState[2] + boardState[4] + boardState[6] === 'XXX') {
+    message.innerHTML = 'PLAYER ONE WIN!'
+    return;
+  } else if (boardState[0] + boardState[1] + boardState[2] === 'OOO' ||
+  boardState[3] + boardState[4] + boardState[5] === 'OOO' ||
+  boardState[6] + boardState[7] + boardState[8] === 'OOO' ||
+  boardState[0] + boardState[3] + boardState[6] === 'OOO' ||
+  boardState[1] + boardState[4] + boardState[7] === 'OOO' ||
+  boardState[2] + boardState[5] + boardState[8] === 'OOO' ||
+  boardState[0] + boardState[4] + boardState[8] === 'OOO' ||
+  boardState[2] + boardState[4] + boardState[6] === 'OOO') {
+    message.innerHTML = 'PLAYER TWO WIN!'
+    return;
+  }
+};
+
+const checkDraw = () => {
+  if (turnCounter === 9) {
+    message.innerHTML ='DRAW!';
+  }
+};
 
 const playerTurn = () => {
   for (let i = 0; i <= 8; i++) {
