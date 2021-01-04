6b054fdfb655983c09d5e5e33fb6eaaae87dc080
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-04 11:30:36 -0800

    Implement playeroneturn script and conditional if row is filled, event cannot be fired

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,26 @@
+let boardState =
+['', '', '',
+'', '', '',
+'','',''];
 
+let turnCounter = 0;
+
+
+const playerOneTurn = () => {
+  for (let i = 0; i <= 8; i++) {
+    var current = document.getElementById(i);
+    current.addEventListener('click', (event) => {
+      if (!event.target.innerText) {
+        console.log('Fired!')
+        event.target.innerHTML = 'X';
+      }
+    });
+  }
+}
+
+const gameStart = () => {
+  playerOneTurn();
+}
+
+gameStart();
 
