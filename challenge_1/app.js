b2295c0f4f295b94c62155ea8334d00e7141964f
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-04 12:27:14 -0800

    Add the sep of concern for player one and two turn. Modify the boardState each turn

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,21 +5,47 @@ let boardState =
 
 let turnCounter = 0;
 
+const playerOneTurn = (i) => {
+  console.log('Player 1 Fired! Board: ' + i);
+  event.target.innerHTML = 'X';
+  boardState[i] = 'X';
+  console.log(boardState);
+  turnCounter ++;
+}
 
-const playerOneTurn = () => {
+const playerTwoTurn = (i) => {
+  console.log('Player 2 Fired! Board: ' + i);
+  event.target.innerHTML = 'O';
+  boardState[i] = 'O';
+  console.log(boardState);
+  turnCounter ++;
+}
+
+const playerTurn = () => {
   for (let i = 0; i <= 8; i++) {
     var current = document.getElementById(i);
     current.addEventListener('click', (event) => {
-      if (!event.target.innerText) {
-        console.log('Fired!')
-        event.target.innerHTML = 'X';
+      if (!event.target.innerText && turnCounter % 2 === 0) {
+        // console.log('Player 1 Fired!' + i);
+        // event.target.innerHTML = 'X';
+        // boardState[i] = 'X';
+        // console.log(boardState);
+        // turnCounter ++;
+        playerOneTurn(i);
+      } else if (!event.target.innerText && turnCounter % 2 === 1) {
+        // console.log('Player 2 fired!' + i);
+        // event.target.innerHTML = 'O';
+        // boardState[i] = 'O';
+        // console.log(boardState);
+        // turnCounter ++;
+        playerTwoTurn(i);
       }
     });
   }
 }
 
 const gameStart = () => {
-  playerOneTurn();
+  playerTurn();
 }
 
 gameStart();
