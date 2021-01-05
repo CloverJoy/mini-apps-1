9ce5f0b3dd43b2bf280d5e7c9ba048fff739c11a
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-05 11:42:20 -0800

    Create a text area, POST request success receiving the JSON

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('client'));
+app.use(express.json())
+app.use(express.urlencoded());
+
 app.get('/', (req, res) => {
   res.send('Online')
+});
+
+app.post('/', (req, res) => {
+  let data = JSON.parse(req.body.json);
+  console.log(data);
+  res.send(data);
+  res.end();
 });
 
 app.listen(port, () => {
