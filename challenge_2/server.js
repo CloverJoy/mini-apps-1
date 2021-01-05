b60aa8fa082a6c8b3395e82569258b04cb59ab23
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-05 13:30:33 -0800

    Create a jsontocsv function in the server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -10,10 +10,36 @@ app.get('/', (req, res) => {
   res.send('Online')
 });
 
+const jsonToCSV = (json) => {
+  let result = '';
+  for (key in json) {
+    if (key !== 'children') {
+      result += `${key}, `
+    }
+  }
+  result = result.slice(0, -2);
+  const flatten = (obj) => {
+    result += '\n';
+    for (const [key, value] of Object.entries(obj)) {
+      if (Array.isArray(value) && value.length !== -1) {
+        for (child of value) {
+          flatten(child);
+        }
+      }
+      if (!Array.isArray(value)) {
+        result += `${value}, `;
+      }
+    };
+    result = result.slice(0, -2).replace('0,', 0);
+  }
+  flatten(json);
+  return result.replace('0,', '0');
+}
+
 app.post('/', (req, res) => {
   let data = JSON.parse(req.body.json);
-  console.log(data);
-  res.send(data);
+  console.log(jsonToCSV(data));
+  res.send(jsonToCSV(data));
   res.end();
 });
 
