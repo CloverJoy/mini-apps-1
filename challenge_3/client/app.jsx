12c13b582a5aa8f4105fca912be40c41728604db
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-06 12:12:26 -0800

    Apply conditional rendering for each form

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,3 +1,29 @@
+const FormTwo = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <label>Address Line 1:
+        <input type="text" name= "lineone" value={props.lineone} onChange={props.handleChange}></input>
+      </label>
+      <label>Address Line 2:
+        <input type="text" name= "linetwo" value={props.linetwo} onChange={props.handleChange}></input>
+      </label>
+      <label>City:
+        <input type="text" name= "city" value={props.city} onChange={props.handleChange}></input>
+      </label>
+      <label>State:
+        <input type="text" name= "state" value={props.state} onChange={props.handleChange}></input>
+      </label>
+      <label>Zip Code:
+        <input type="text" name= "zipcode" value={props.zipcode} onChange={props.handleChange}></input>
+      </label>
+      <label>Phone number:
+        <input type="text" name= "phonenumber" value={props.phonenumber} onChange={props.handleChange}></input>
+      </label>
+      <input type="submit" value="submit"></input>
+    </form>
+  );
+};
+
 const FormOne = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
@@ -8,7 +34,7 @@ const FormOne = (props) => {
         <input type="text" name= "email" value={props.email} onChange={props.handleChange}></input>
       </label>
       <label>Password:
-        <input type="text" name= "password" value={props.password} onChange={props.handleChange}></input>
+        <input type="password" name= "password" value={props.password} onChange={props.handleChange}></input>
       </label>
       <input type="submit" value="submit"></input>
     </form>
@@ -22,8 +48,8 @@ class App extends React.Component {
       name: '',
       email: '',
       password: '',
-      line1: '',
-      line2: '',
+      lineone: '',
+      linetwo: '',
       city: '',
       state: '',
       zipcode: '',
@@ -51,12 +77,35 @@ class App extends React.Component {
     event.preventDefault();
   }
 
+  componentDidMount() {
+    this.setState({step: 0});
+  }
+
 
   render() {
-    return (
-      <FormOne name={this.state.name} email={this.state.email} password={this.state.password} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
-    );
-  }
+    let currentStep = this.state.step
+    if (currentStep === 0) {
+      return (
+        <FormOne name={this.state.name} email={this.state.email} password={this.state.password} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+      )
+    } else if (currentStep === 1) {
+      return (
+        <FormTwo lineone={this.state.lineone} linetwo={this.state.linetwo} city={this.state.city} state={this.state.state} zipcode={this.state.zipcode} phonenumber={this.state.phonenumber} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+      )
+    } else {
+      return (
+        <div>
+        <div>
+        <FormOne name={this.state.name} email={this.state.email} password={this.state.password} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+        </div>
+        <div>
+        <FormTwo lineone={this.state.lineone} linetwo={this.state.linetwo} city={this.state.city} state={this.state.state} zipcode={this.state.zipcode} phonenumber={this.state.phonenumber} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+        </div>
+        </div>
+      );
+    }
+    }
+
 };
 
 
