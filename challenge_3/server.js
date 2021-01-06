34991e05d5dc4fc3a443590793aa17c8a792e794
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-06 10:35:31 -0800

    Set up react dev environment and render hello world in react way for testing

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 1234;
 
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Multistep Checkout is now listening at http://localhost:${port}`);
+});
 
